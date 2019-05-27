bcc937485de994d2d16293d0182d7abfd9edb02b
Asvarizal Filcha <asvarizal.filcha@students.amikom.ac.id>
2019-05-27 10:33:50 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 $host	= "localhost";
 $user	= "root";
 $pass	= "";
-$db	    = "db_kampus";
+$db	  = "db_kampus";
 
 //Menggunakan objek mysqli untuk membuat koneksi dan menyimpan nya dalam variabel $mysqli	
 $mysqli = new mysqli($host, $user, $pass, $db);
